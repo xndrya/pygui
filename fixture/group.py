5086a1465293994b9f3cd7aa92ded4e10213b826
X <xndrya@gmail.com>
2019-07-21 00:18:55 +0300

    Получение списка групп и добавление новой

---
+++ fixture/group.py
@@ -0,0 +1,26 @@
+class GroupHelper:
+    def __init__(self, app):
+        self.app = app
+
+    def get_group_list(self):
+        group_list = []
+        self.open_group_editor()
+        tree = self.group_editor.window(auto_id = "uxAddressTreeView")
+        root = tree.tree_root
+        group_list = [node.text() for node in root_children()]
+        self.close_group_editor()
+        return group_list
+
+    def add_new_group(self, name):
+        self.open_group_editor()
+        self.group_editor.window(auto_id = "uxNewAddressButton").click()
+        self.group_editor.window(class_name="Edit").set_text(name).type_keys("\n")
+        self.close_group_editor()
+
+    def open_group_editor(self):
+        self.app.main_window.window(auto_id = "groupButton").click()
+        self.group_editor = self.app.application.window(title =  "Group editor")
+        self.group_editor.wait("visible")
+
+    def close_group_editor(self):
+        self.group_editor.close()
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,2 +1,6 @@
 def test_add_group(app):
-    pass
+    old_list = app.groups.get_group_list()
+    app.groups.add_new_group("Test")
+    new_list = app.groups.get_group_list()
+    old_list.append("Test")
+    assert sorted(old_list) == sorted(new_list)
